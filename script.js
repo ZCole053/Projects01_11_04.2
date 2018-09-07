5a3af5cbc128dce25a9b6450f24519b8b88b29e2
Unknown <zcole053@west-mec.org>
2018-09-07 14:24:45 -0700

    Radio buttons
    I am adding the radio buttons in the html to check the difference from zip codes in the U.S. and Germany. I added another global variable along with a function and event listener for button validity. Finally I added a function that takes the data and displays it.

--- script.js
+++ script.js
@@ -10,6 +10,7 @@
 
 // Global Variables
 var httpRequest = false;
+var countrySel;
 
 // this function allows us to contact other servers for data
 function getRequestObject(){
@@ -41,6 +42,11 @@ function checkInput() {
     }
 }
 
+// this function checks when the buttons are selected
+function checkButtons(){
+
+}
+
 // this gets the location of the zip
 function getLocation(){
     var zip = document.getElementById("zip").value;
@@ -48,9 +54,23 @@ function getLocation(){
         httpRequest = getRequestObject();
     }
     httpRequest.abort();
-    httpRequest.open("get","http//api.zippopotam.us/us/" + zip, true);
+    httpRequest.open("get","http://api.zippopotam.us/us/" + zip, true);
     httpRequest.send(null);
     httpRequest.onreadystatechange = displayData;
+}
+
+
+// this function takes the data and displays it into the Dom
+function displayData(){
+    if(httpRequest.readyState === 4 && httpRequest.status ===200){
+        var resultData = JSON.parse(httpRequest.responseText);
+        var city = document.getElementById("city");
+        var state = document.getElementById("state");
+        city.value = resultData.places[0]["place name"];
+        state.value = resultData[0]["state abbreviation"];
+        document.getElementById("zip").blur();
+        document.getElementById("casset").style.visibility = "visible";
+    }
 }
 
 
@@ -60,4 +80,14 @@ if (zip.addEventListener) {
     zip.addEventListener("keyup", checkInput, false);
 } else if (zip.attachEvent) {
     zip.attachEvent("onkeyup", checkInput);
+}
+
+var germany = document.getElementById("germany");
+var us = document.getElementById("us");
+if (us.addEventListener) {
+    us.addEventListener("keyup", checkButtons, false);
+    germany.addEventListener("keyup", checkButtons, false);
+} else if (us.attachEvent) {
+    us.attachEvent("onkeyup", checkButtons);
+    germany.attachEvent("onkeyup", checkButtons);
 }
